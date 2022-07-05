6dc9fe2bada29e85812baaeeec4bc034d2784cec
jinwei69 <ss610349@gmail.com>
2022-07-05 08:59:43 +0800

    更新websocket host  預設值

--- src/web-api/Backend/NetworkConfigsAPI.js
+++ src/web-api/Backend/NetworkConfigsAPI.js
@@ -4,8 +4,8 @@ export async function GetNetworkConfigs() {
     console.error('從後端取得網路參數配置失敗,使用預設值')
     return {
       data: {
-        controlCenterWsHost: 'ws://192.168.0.201:8090',
-        idmsWsHost: 'ws://192.168.0.201:44332',
+        controlCenterWsHost: 'ws://localhost:8090',
+        idmsWsHost: 'ws://localhost:44332',
       },
     }
   })
